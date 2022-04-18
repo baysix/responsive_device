22c6071c73f3701fe4ac98d3b2c9bb5d00c352f4
yspark1 <yspark1@enliple.com>
2022-04-18 18:46:18 +0900

    [1차커밋]2

---
+++ src/pages/main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Main = () =>{
+    return(
+        <div>
+            <div>t
+
+            </div>
+            <div>
+                test
+            </div>
+        </div>
+    )
+}
+
+export default Main;
