46418090f386015600fc9c393968498f13fc8c1f
yspark1 <yspark1@enliple.com>
2022-12-12 17:07:52 +0900

    체리픽 테스트222

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//체리픽 테스트222
+//체리픽 테스트222444
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
